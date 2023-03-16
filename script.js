e7b34d0a8636cca2e5727cd9296bf70559a03856
Nyaromollo <omolloenter@gmail.com>
2023-03-16 20:43:18 +0300

    Work Done

--- script.js
+++ script.js
@@ -1,4 +1,7 @@
 const h2 = document.createElement("h2");
-h2.textContent = "This content added by JavaScript";
+h2.textContent = "My stunning world";
 
 document.querySelector("body").appendChild(h2);
+
+function handleclick(){
+    alert("Clicked");
